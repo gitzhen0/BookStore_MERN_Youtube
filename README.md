1cd9e8a55bdf49380a6ddd2dd5762fb184798e96
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2024-03-03 13:33:34 -0500

    connected to mongo, backend is running

---
+++ README.md
@@ -0,0 +1,8 @@
+* completeness: 
+* start date: 3/3/2023
+* Youtube link: https://www.youtube.com/watch?v=-42K44A1oMA
+* setup to run again:
+  1. a free mongodb server
+  2. reinstall npm packages
+     1. `cd frontend` + `npm install`
+     2. `cd backend` + `npm install`
---
+++ backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@books-store-mern.qlw8nth.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN";
+
+//database mongoDBurl needs to be updated to run the app again
+// you need to go to mongodb altas create a mongodb again.
+//current db will be deleted upon completion of this project
