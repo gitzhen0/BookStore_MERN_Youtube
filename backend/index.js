55367830118fb40a4ca7e40138a6f6a08def5134
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2024-03-03 17:52:58 -0500

    basic home page done

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,16 @@ const app = express();
 app.use(express.json());
 
 // option1: allow all origins with defaul tof cors(*)
-// app.use(cors());
+app.use(cors());
 
 // option2: allow custom origins
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (request, response) => {
   console.log(request);
